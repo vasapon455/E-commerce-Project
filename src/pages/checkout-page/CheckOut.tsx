51635fcb1cb84ec664bd4b041e40777091ac048f
Vasapon <vasapon1996@gmail.com>
2023-12-14 14:03:56 +0700

    Conditional Rendering ในหน้า Check Out

--- src/pages/checkout-page/CheckOut.tsx
+++ src/pages/checkout-page/CheckOut.tsx
@@ -7,8 +7,8 @@ export default function CheckOut(){
     
     return(
         
-        <div>
-            {itemsInCart.map(item => <CheckoutItem key={item.id.toString()} product={item} />)}
+        <div className=" grid-cols-1">
+            { itemsInCart.length > 0? itemsInCart.map(item => <CheckoutItem key={item.id.toString()} product={item}/>) : <p className=" text-black">You don't have any item in cart.</p> }
         </div>
     );
 }
