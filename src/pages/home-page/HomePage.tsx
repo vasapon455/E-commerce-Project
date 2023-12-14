e125695210763f66c96931d9d3c722067ba70d23
Vasapon <vasapon1996@gmail.com>
2023-12-14 11:52:30 +0700

    ปรับหน้า homepage แลพ Product Page

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -3,7 +3,7 @@ import HeroBanner from './HeroBanner'
 export default function HomePage() {
   return (
     <div className="flex justify-center">
-      <div className="pt-10 m-10  w-1024 text-center bg-blue-900 text-white">
+      <div className="pt-10 m-10  w-1024 text-center bg-blue-900 text-white mt-20">
             <header className=" mb-20 h-11 ">
                 <h1 className="text-6xl font-extrabold text-center">Busy Shop</h1>
                 <HeroBanner/>
--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -9,7 +9,7 @@ export default function ProductsPage() {
   
   return (
     <div className="flex justify-center">
-      <div className="pt-10 m-10 bg-blue-900 text-white w-1024 text-center align-middle">
+      <div className="pt-10 m-20 bg-blue-900 text-white w-1024 text-center align-middle">
         <header className=" mb-20 h-11 "><h1 className="text-6xl font-extrabold text-center">All Products</h1></header>   
         <div className="grid grid-cols-2 gap-4 p-4">
         {products.map((product)=><ProductItemComponent key={product.id.toString()} product={product}/>) }
