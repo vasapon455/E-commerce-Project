4a4215689c590ab60bd7d274875047067d38d59f
Vasapon <vasapon1996@gmail.com>
2023-12-14 11:37:37 +0700

    ออกแบบ NavBar ให้ fixed & w-full

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 
 import CartCounter from "./CartCounter"
 import {Link} from "react-router-dom"
+import "./NavBar.css"
 
 export default function NavBar() {
   return (
-    <nav className="bg-blue-900 text-white shadow-lg text-center">
+    <nav className="bg-blue-900 text-white shadow-lg text-center fixed w-full">
       <div className="inline-flex justify-center gap-80">
         <div className="flex gap-5">
           <Link to="/" className="text-1m font-bold p-3 hover:bg-red-800">Home</Link>
