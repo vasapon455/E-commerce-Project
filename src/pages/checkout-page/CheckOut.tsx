bb589d3718661334e72e4f518329b2a0f5d9423d
Vasapon <vasapon1996@gmail.com>
2023-12-14 16:09:23 +0700

    add functionality to checkout page

--- src/pages/checkout-page/CheckOut.tsx
+++ src/pages/checkout-page/CheckOut.tsx
@@ -4,11 +4,27 @@ import CheckoutItem from "./CheckoutItem";
 export default function CheckOut(){
 
     const {itemsInCart} = useCart();
-    
+    const itemPrice = itemsInCart.map((item)=> item.price);
+    const allPrice =itemPrice.reduce((prev,current)=>{return prev + current},0);
+
     return(
-        
-        <div className=" grid-cols-1">
-            { itemsInCart.length > 0? itemsInCart.map(item => <CheckoutItem key={item.id.toString()} product={item}/>) : <p className=" text-black">You don't have any item in cart.</p> }
-        </div>
+        <div className="flex flex-col justify-center  items-center">
+            <div className="pt-10 m-20 bg-blue-900 text-white w-1024 text-center align-middle">
+             <header className=" mb-20 h-11 "><h1 className="text-6xl font-extrabold text-center">Check Out</h1></header>   
+           
+            <div className="grid-cols-1 ">
+                { itemsInCart.length > 0 && itemsInCart.map(item => <CheckoutItem key={item.id.toString()} product={item}/>) }
+            </div>
+            <p className={itemsInCart.length===0?"text-white":"invisible"}>You don't have any product in cart.</p>
+            <div className="flex flex-col justify-around ">
+                    <p className="text-white text-xl m-8 "><span className="font-bold">Total Price:  </span>${allPrice}</p>
+                    <button
+                    className={itemsInCart.length===0?"invisible" : "bg-red-800 hover:bg-red-950 text-white font-bold py-2 px-4 rounded-xl m-8"}
+            >
+                    Submit Order
+                </button>
+            </div>
+            </div>
+        </div>    
     );
-}
+} 
--- src/pages/checkout-page/CheckoutItem.tsx
+++ src/pages/checkout-page/CheckoutItem.tsx
@@ -13,17 +13,21 @@ export default function CheckoutItem({product}:CheckOutItemProps){
     return (
         <div>
             <div className="flex justify-center">
-            <div className="pt-10 m-10  w-1024">
-                <div className="rounded-lg shadow-md p-4  border border-gray-300">
+             <div className="m-2  w-1024">
+                <div className="text-black text-left text-lg bg-white rounded-lg shadow-md p-4  border border-gray-300">
                     <h2 className="text-xl font-bold mb-4">{product?.title}</h2>
-                    <div className="flex-col justify-center m-8 ">
-                        <div className="flex justify-center"><img src={product?.image} alt={product?.description} className="w-80 m-8 "/></div>
+                    <div className="flex justify-center text-left m-8 ">
+                        <img src={product?.image} alt={product?.description} className=" w-40 h-auto m-4 "/>
                         <div className="text-m m-8 "><span className="font-bold">Description:  </span>{product?.description}</div>
-                        <div className="flex flex-row justify-between">
-                            <p className="p-2 mx-5"><span className="font-bold">Price:  </span>${product?.price}</p>
-                        </div>
+                        <div className="flex flex-col p-0 m-0 justify-center text-center">
+                        <p className=" text-2xl m-8"><span className="font-bold">Price:  </span>${product?.price}</p>
+                            <button
+                    className=" bg-red-800 hover:bg-red-950 text-white font-bold py-2 px-4 rounded-xl my-4 mt-8"
+            >
+                    Remove
+                </button>
+            </div>
                     </div>
-                   
                 </div>
             </div>
         </div>
